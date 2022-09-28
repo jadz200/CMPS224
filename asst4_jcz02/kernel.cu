7a3926398b6038108a1609d300fd1a0b4ee2a303
jadz200 <jadzarzour3@gmail.com>
2022-09-28 16:52:05 +0300

    ayre

--- asst4_jcz02/kernel.cu
+++ asst4_jcz02/kernel.cu
@@ -8,19 +8,14 @@
 __constant__ float filter_c[FILTER_DIM][FILTER_DIM];
 
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
-
-    // TODO
-
-
-
-
-
-
-
-
-
-
-
+	// TODO
+	__shared__ float in_s[IN_TILE_DIM][IN_TILE_DIM]; 
+	int row= blockIdx.y*OUT_TILE_DIM + threadIdx.y;
+	int col = blockIdx.x*OUT_TILE_DIM + threadIdx.x;
+	if(outRow<height && outCol<width){
+		float sum= 0.0f;
+	}
+	__syncthreads();
 
 
 
@@ -39,10 +34,10 @@ __global__ void convolution_tiled_kernel(float* input, float* output, unsigned i
 
 void copyFilterToGPU(float filter[][FILTER_DIM]) {
 
-    // Copy filter to constant memory
-
+    // Copy filter to constant memoryi
+	
     // TODO
-
+    cudaMemcpyToSymbol(filter_c,filter,FILTER_DIM*FILTER_DIM*sizeof(float));
 }
 
 void convolution_tiled_gpu(float* input_d, float* output_d, unsigned int width, unsigned int height) {
@@ -50,8 +45,9 @@ void convolution_tiled_gpu(float* input_d, float* output_d, unsigned int width, 
     // Call kernel
 
     // TODO
-
-
+	dim3 numThreadsPerBlock(IN_TILE_DIM,IN_TILE_DIM);
+	dim3 numBlocks((width + IN_TILE_DIM - 1) / IN_TILE_DIM ,(height + IN_TILE_DIM - 1) / IN_TILE_DIM  );
+	convoltuion_tiled_kernel<<< numBlocks, numThreadsPerBlock >>>(input_d, output_d, width, height);
 
 }
 
