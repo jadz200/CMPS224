ac28db6fa41907806ef5d03622983c0a66850612
jadz200 <jadzarzour3@gmail.com>
2022-10-05 19:31:29 +0300

    fixed coarsening

--- asst5_jcz02/kernel.cu
+++ asst5_jcz02/kernel.cu
@@ -30,23 +30,22 @@ void histogram_gpu_private(unsigned char* image_d, unsigned int* bins_d, unsigne
 
 __global__ void histogram_private_coarse_kernel(unsigned char* image, unsigned int* bins, unsigned int width, unsigned int height) {
 
-    __shared__  int  b_s[NUM_BINS];
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    if(threadIdx.x<NUM_BINS){
-	b_s[threadIdx.x]=0;
-    }
+    __shared__ unsigned int bins_s [NUM_BINS];
+    unsigned int index = blockIdx.x * blockDim.x * COARSE_FACTOR + threadIdx.x;
+
+    if ( threadIdx.x < NUM_BINS ) 
+        bins_s[ threadIdx.x ] = 0;
+    
     __syncthreads();
-    for(unsigned int c=0;c<COARSE_FACTOR;c++){
-	if(i+blockDim.x*c<width*height){
-		unsigned char b=image[i+blockDim.x*c];
-		atomicAdd(&(bins[b]),1);
-	}
-    }
+
+    for(int i = 0; i < COARSE_FACTOR; ++i) 
+        if (i * blockDim.x + index < width * height)
+            atomicAdd(&bins_s[image[i * blockDim.x + index ]], 1);
+
     __syncthreads();
-   if(threadIdx.x< NUM_BINS && b_s[threadIdx.x]>0){
-	atomicAdd(&(bins[threadIdx.x]),b_s[threadIdx.x]);
-   }
-	
+
+    if (threadIdx.x < NUM_BINS && bins_s[threadIdx.x] > 0) 
+        atomicAdd(&bins[threadIdx.x], bins_s[threadIdx.x]);
 }
 
 void histogram_gpu_private_coarse(unsigned char* image_d, unsigned int* bins_d, unsigned int width, unsigned int height) {
