d68ee453b2b14e168c990859463868c2ba97d088
jadz200 <jadzarzour3@gmail.com>
2022-10-12 19:08:27 +0300

    fixed kernel

--- asst6_jcz02/kernel.cu
+++ asst6_jcz02/kernel.cu
@@ -6,19 +6,21 @@
 
 __global__ void reduce_kernel(float* input, float* sum, unsigned int N) {
     __shared__ float input_s[BLOCK_DIM];
-    
-    unsigned int i = N / 2 + blockIdx.x * BLOCK_DIM + threadIdx.x;
-    
+    unsigned int i = 2*blockIdx.x * BLOCK_DIM + threadIdx.x;
+
     if (i < N) 
-        input_s[threadIdx.x] = input[i] + input[blockIdx.x * BLOCK_DIM + threadIdx.x];
+        input_s[threadIdx.x] = input[i] ;
+    else input_s[threadIdx.x] = 0.0f;
+    if((i-BLOCK_DIM)<N) 
+        input_s[threadIdx.x] += input[i-BLOCK_DIM] ;
     __syncthreads();
 
     for(unsigned int stride = BLOCK_DIM / 2; stride > 0; stride /=2) {
-        if (threadIdx.x >= stride ) 
+        if (threadIdx.x >=(BLOCK_DIM- stride) ) 
             input_s[threadIdx.x] += input_s[threadIdx.x - stride];
         __syncthreads(); 
     }
-    if (threadIdx.x == 0)
+    if (threadIdx.x == (BLOCK_DIM-1))
         atomicAdd(sum, input_s[BLOCK_DIM - 1]);
 }
 
